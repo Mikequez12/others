c34d49ce3fecd4aa40703207fb6660fd82525087
Mike <142720633+Mikequez12@users.noreply.github.com>
2025-03-20 14:38:56 +0100

    Create chromium.py

---
+++ chromium.py
@@ -0,0 +1,16 @@
+import time
+import random
+import os
+
+l = 'abcdefghijklmnñopqrtstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890'
+
+def getRandom(): return l[random.randint(0,len(l)-1)
+
+for i in range(1000):
+  print(getRandom(),end='')
+
+  time.sleep(random.randint(0,100)/100)
+
+os.system('clear')
+
+print('Abort [done]')
