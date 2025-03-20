19f3a100983d639a7f47d1ca39dbcf973c82fb30
Mike <142720633+Mikequez12@users.noreply.github.com>
2025-03-20 14:47:52 +0100

    Update chromium.py

--- chromium.py
+++ chromium.py
@@ -7,8 +7,12 @@ l = 'abcdefghijklmnñopqrtstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890'
 def getRandom():
   return l[random.randint(0,len(l)-1)]
 
+arr = []
+
 for i in range(1000):
-  print(getRandom(),end='')
+  os.system('clear')
+  arr.append(getRandom())
+  os.system('echo' + ''.join(arr))
 
   time.sleep(random.randint(0,100)/100)
 
